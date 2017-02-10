2afa6c5c1d8620faf7c45546142fcf5b98749be8
Shalem Raj <shalemraj93@gmail.com>
2017-02-10 15:26:50 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,16 +1,15 @@
 public class Cred {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+	{
 		String cred[] = credn().split("-");
 		
 		System.out.println(cred[0]);
 		System.out.println(cred[1]);
-		
 	}
 
-	public static String credn() {
-		
+	public static String credn() 
+	{
 		String usr = "username";
 		String pass = "password";
 			
